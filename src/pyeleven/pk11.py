b849c064307d0b8b4f9b6b3c474f8b84a53ef5cc
Fredrik Thulin <fredrik@thulin.net>
2017-01-17 15:01:18 +0100

    Don't fail login if the error is 'already logged in'.

--- src/pyeleven/pk11.py
+++ src/pyeleven/pk11.py
@@ -124,7 +124,12 @@ class SessionInfo(object):
         if slot not in sessions:
             session = lib.openSession(slot)
             if pin is not None:
+                try:
                 session.login(pin)
+                except PyKCS11.PyKCS11Error as ex:
+                    logging.debug('Login failed: {!r}'.format(ex))
+                    if 'CKR_USER_ALREADY_LOGGED_IN' not in str(ex):
+                        raise
             si = SessionInfo(session=session, slot=slot)
             sessions[slot] = si
         # print "opened session for %s:%d" % (lib, slot)
