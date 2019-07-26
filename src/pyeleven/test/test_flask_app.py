0fe48e12f8ca129756af45ee000fb08414372ff1
Johan Lundberg <lundberg@sunet.se>
2019-07-26 16:13:11 +0200

    Broke out tests to easier test one at the time

---
+++ src/pyeleven/test/test_flask_app.py
@@ -0,0 +1,150 @@
+"""
+Testing the PKCS#11 shim layer
+"""
+import os
+import time
+from base64 import b64encode
+from unittest import TestCase
+
+import six
+from flask import json
+
+from pyeleven.test import P11_MODULE
+from pyeleven.test.utils import TemporarySoftHSM
+
+__author__ = 'leifj'
+
+
+class FlaskTestCase(TestCase):
+    softhsm = TemporarySoftHSM.get_instance()
+
+    def setUp(self):
+        from .. import app
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        app.config['TESTING'] = True
+        app.config['PKCS11MODULE'] = P11_MODULE
+        app.config['PKCS11PIN'] = 'secret1'
+        self.app = app.test_client()
+
+    #@classmethod
+    #def tearDownClass(cls):
+    #    cls.softhsm.clean_up()
+
+    def test_info(self):
+        rv = self.app.get("/info")
+        self.assertIsNotNone(rv.data)
+        d = json.loads(rv.data)
+        self.assertIsNotNone(d)
+        self.assertIn('library', d)
+        self.assertEqual(d['library'], P11_MODULE)
+
+    def test_sign(self):
+        data = b64encode(b"test")
+        if six.PY3:
+            data = data.decode('utf-8')
+        rv = self.app.post("/test/test/sign",
+                           content_type='application/json',
+                           data=json.dumps(dict(mech='RSAPKCS1', data=data)))
+        self.assertIsNotNone(rv.data)
+        d = json.loads(rv.data)
+        self.assertIsNotNone(d)
+        self.assertIn('slot', d)
+        self.assertIn('signed', d)
+
+    def test_bad_sign(self):
+        exception_thrown = False
+        try:
+            data = b64encode(b"test")
+            if six.PY3:
+                data = data.decode('utf-8')
+            rv = self.app.post("/test/doesnotexist/sign",
+                               content_type='application/json',
+                               data=json.dumps(dict(mech='RSAPKCS1', data=data)))
+        except Exception as ex:
+            exception_thrown = True
+            from traceback import print_exc
+            if six.PY2:
+                print_exc(ex)
+            else:
+                print_exc()
+        self.assertTrue(exception_thrown)
+
+    def test_1000_sign(self):
+        ts = time.time()
+        data = b64encode(b"test")
+        if six.PY3:
+            data = data.decode('utf-8')
+        for i in range(0, 999):
+            rv = self.app.post("/test/test/sign",
+                               content_type='application/json',
+                               data=json.dumps(dict(mech='RSAPKCS1', data=data)))
+            self.assertIsNotNone(rv.data)
+            d = json.loads(rv.data)
+            self.assertIsNotNone(d)
+            self.assertIn('slot', d)
+            self.assertIn('signed', d)
+        te = time.time()
+        print("1000 signatures (http): %2.3f sec (speed: %2.5f s/sig)" % (te - ts, (te - ts) / 1000))
+
+    def test_label_sign(self):
+        data = b64encode(b"test")
+        if six.PY3:
+            data = data.decode('utf-8')
+        rv = self.app.post("/test/test/sign",
+                           content_type='application/json',
+                           data=json.dumps(dict(mech='RSAPKCS1', data=data)))
+        self.assertIsNotNone(rv.data)
+        d = json.loads(rv.data)
+        self.assertIsNotNone(d)
+        self.assertIn('slot', d)
+        self.assertIn('signed', d)
+        self.assertIsNotNone(d['signed'])
+
+    def test_bad_sign_request(self):
+        try:
+            rv = self.app.post("/test/test/sign",
+                               content_type='application/json',
+                               data=json.dumps('foo'))
+            assert False
+        except ValueError:
+            pass
+
+    def test_slot_info(self):
+        rv = self.app.get("/test")
+        self.assertIsNotNone(rv.data)
+        d = json.loads(rv.data)
+        self.assertIsNotNone(d)
+        self.assertIn('slots', d)
+        for nfo in d['slots']:
+            self.assertIn('mechanisms', nfo)
+            self.assertIn('slot', nfo)
+            self.assertIn('token', nfo)
+            self.assertIn('manufacturerID', nfo['slot'])
+            self.assertIn('SoftHSM', nfo['slot']['manufacturerID'])
+            self.assertIn('label', nfo['token'])
+            self.assertIn('test', nfo['token']['label'])
+
+    def test_token_info(self):
+        rv = self.app.get("/")
+        assert rv.data
+        d = json.loads(rv.data)
+        self.assertIsNotNone(d)
+        self.assertIn('slots', d)
+        self.assertEqual(len(d['slots']), 2)
+        self.assertIn(1, d['slots'])
+        self.assertIn(0, d['slots'])
+        self.assertIn('labels', d)
+        self.assertIn('test', d['labels'])
+        test_slots = d['labels']['test']
+        self.assertEqual(len(test_slots), 2)
+        self.assertIn(1, test_slots)
+        self.assertIn(0, test_slots)
+
+    def test_slot_objects(self):
+        rv = self.app.get("/test/objects")
+        assert rv.data
+        d = json.loads(rv.data)
+        self.assertIsNotNone(d)
+        self.assertIn('session', d)
+        self.assertIn('objects', d)
+        self.assertNotEqual(d['objects'], [])
---
+++ src/pyeleven/test/test_pkcs11.py
@@ -0,0 +1,168 @@
+import os
+import random
+import time
+from unittest import TestCase
+
+import pkg_resources
+from retrying import retry
+
+from pyeleven import pk11
+from pyeleven.test import P11_MODULE
+from pyeleven.test.utils import TemporarySoftHSM
+from pyeleven.test.utils import ThreadPool
+from pyeleven.utils import mechanism, intarray2bytes
+
+
+class TestPKCS11(TestCase):
+    softhsm = TemporarySoftHSM.get_instance()
+
+    def setUp(self):
+        datadir = pkg_resources.resource_filename(__name__, 'data')
+
+    #@classmethod
+    #def tearDownClass(cls):
+    #    cls.softhsm.clean_up()
+
+    def test_open_session(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        with pk11.pkcs11(P11_MODULE, 'test', "secret1") as session:
+            self.assertIsNotNone(session)
+
+    def test_multislot(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        lib = pk11.load_library(P11_MODULE)
+        slots = pk11.slots_for_label('test', lib)
+        self.assertIn(1, slots)
+        self.assertIn(0, slots)
+        self.assertEqual(len(slots), 2)
+
+    def test_find_key(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        with pk11.pkcs11(P11_MODULE, 'test', "secret1") as si:
+            self.assertIsNotNone(si)
+            key, cert = si.find_key('test')
+            self.assertIsNotNone(key)
+            self.assertIsNotNone(cert)
+
+    def test_find_key_spread(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        hits = {0: 0, 1: 0}
+
+        @retry(stop_max_attempt_number=20)
+        def _try_sign():
+            with pk11.pkcs11(P11_MODULE, 'test', 'secret1') as si:
+                self.assertIsNotNone(si)
+                key, cert = si.find_key('test')
+                self.assertIsNotNone(key)
+                self.assertIsNotNone(cert)
+                self.assertIsNotNone(si.slot)
+                hits[si.slot] += 1
+                if si.slot == random.choice([0, 1]):
+                    raise ValueError("force a retry...")
+
+        for i in range(0, 99):
+            _try_sign()
+        self.assertGreater(hits[0], 30)
+        self.assertGreater(hits[1], 30)
+
+    def test_find_key_by_label(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        with pk11.pkcs11(P11_MODULE, 'test', "secret1") as si:
+            self.assertIsNotNone(si)
+            key, cert = si.find_key('test')
+            self.assertIsNotNone(key)
+            self.assertIsNotNone(cert)
+
+    def test_exception_reopen_session(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        for i in range(0, 10):
+            try:
+                with pk11.pkcs11(P11_MODULE, 'test', "secret1") as si:
+                    self.assertIsNotNone(si)
+                    raise ValueError("oops...")
+            except ValueError:
+                pass
+
+    def test_sign(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        with pk11.pkcs11(P11_MODULE, 'test', "secret1") as si:
+            key, cert = si.find_key('test')
+            signed = intarray2bytes(si.session.sign(key, 'test', mechanism('RSAPKCS1')))
+            self.assertIsNotNone(signed)
+
+    def test_1000_sign(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+        print('test_1000_sign')
+
+        ts = time.time()
+        for i in range(0, 999):
+            print('test_1000_sign_%d' % i)
+            with pk11.pkcs11(P11_MODULE, 'test', 'secret1') as si:
+                print(si)
+                self.assertIsNotNone(si)
+                key, cert = si.find_key('test')
+                self.assertIsNotNone(key)
+                self.assertIsNotNone(cert)
+                signed = intarray2bytes(si.session.sign(key, 'test', mechanism('RSAPKCS1')))
+                self.assertIsNotNone(signed)
+        te = time.time()
+        print("1000 signatures (p11): %2.3f sec (speed: %2.5f sec/s)" % (te - ts, (te - ts) / 1000))
+
+    def test_stress_sign_sequential(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+
+        def _sign(msg):
+            with pk11.pkcs11(P11_MODULE, 'test', "secret1") as si:
+                key, cert = si.find_key('test')
+                signed = intarray2bytes(si.session.sign(key, msg, mechanism('RSAPKCS1')))
+                self.assertIsNotNone(signed)
+
+        for i in range(0, 999):
+            _sign("message %d" % i)
+
+    def test_stress_sign_parallell_20(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+
+        def _sign(msg):
+            with pk11.pkcs11(P11_MODULE, 'test', "secret1") as si:
+                key, cert = si.find_key('test', find_cert=False)
+                signed = intarray2bytes(si.session.sign(key, msg, mechanism('RSAPKCS1')))
+                self.assertIsNotNone(signed)
+
+        ts = time.time()
+        tp = ThreadPool(20)
+        for i in range(0, 999):
+            tp.add_task(_sign, "message %d" % i)
+        tp.wait_completion()
+        te = time.time()
+        print("1000 signatures (p11 parallell): %2.3f sec (speed: %2.5f sec/s)" % (te - ts, (te - ts) / 1000))
+
+    def test_stress_sign_parallell_20_with_failovers(self):
+        os.environ['SOFTHSM2_CONF'] = self.softhsm.softhsm_conf
+        pk11.reset()
+
+        @retry(stop_max_attempt_number=10)
+        def _sign(i):
+            msg = "message %d" % i
+            with pk11.pkcs11(P11_MODULE, 'test', "secret1") as si:
+                key, _ = si.find_key('test', find_cert=False)
+                signed = intarray2bytes(si.session.sign(key, msg, mechanism('RSAPKCS1')))
+                self.assertIsNotNone(signed)
+
+        ts = time.time()
+        tp = ThreadPool(20)
+        for i in range(0, 999):  # simulate 10 failures on each slot
+            tp.add_task(_sign, i)
+        tp.wait_completion()
+        te = time.time()
+        print("1000 signatures (p11 parallell): %2.3f sec (speed: %2.5f sec/s)" % (te - ts, (te - ts) / 1000))
